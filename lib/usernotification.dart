528b13ac5f0e5b8744c12052163283359d4fe46f
Adarsh C M <11200049@rist.edu.in>
2024-05-01 20:47:58 +0530

    data fetch and display added

---
+++ lib/usernotification.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+//auth
+import 'package:firebase_auth/firebase_auth.dart';
+//firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:xamify/notificationdetails.dart';
+//transitions.dart
+import 'package:xamify/transitions.dart';
+
+class UserNotification extends StatefulWidget {
+  const UserNotification({super.key});
+
+  @override
+  State<UserNotification> createState() => _UserNotificationState();
+}
+
+class _UserNotificationState extends State<UserNotification> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff1E7BC5),
+        title: Image.asset('assets/name.png', height: 24),
+        centerTitle: true,
+        //back button colour white
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                'All Notifications',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              const SizedBox(height: 10),
+              //get all title and head from collection userdata doc email subcollection notification
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('userdata')
+                    .doc(FirebaseAuth.instance.currentUser!.email)
+                    .collection('notification')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return Column(
+                    children: snapshot.data!.docs.map((doc) {
+                      var data = doc.data() as Map<String, dynamic>;
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 5),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                FadeRoute(
+                                    page: NotificationDetails(docid: doc.id)));
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            width: double.infinity,
+                            height: 55,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: const BorderSide(
+                                    width: 1, color: Color(0xFFBFDAEF)),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      data['title'],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 13,
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.w700,
+                                      ),
+                                    ),
+                                    Text(
+                                      data['head'],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 13,
+                                        fontFamily: 'Montserrat',
+                                        fontWeight: FontWeight.w200,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Image.asset('assets/view.png', height: 12),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
