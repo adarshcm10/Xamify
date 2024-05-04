d77b97c88444c02e2b1534ff2ef52dcd0b2c9aac
Adarsh C M <11200049@rist.edu.in>
2024-05-04 22:16:26 +0530

    data fetch and exam add functions added

---
+++ lib/search.dart
@@ -0,0 +1,381 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+//auth
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  bool _isSearching = false;
+  final TextEditingController _searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff1E7BC5),
+        title: Image.asset('assets/name.png', height: 24),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //search bar
+              Row(
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: TextField(
+                      controller: _searchController,
+                      cursorColor: Colors.white,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.w400),
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        hintStyle: const TextStyle(
+                            color: Color.fromARGB(88, 255, 255, 255),
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400),
+                        contentPadding: const EdgeInsets.only(left: 20),
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 1),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  //search button gesturedetector
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isSearching = true;
+                      });
+                    },
+                    child: Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width * 0.13,
+                      decoration: BoxDecoration(
+                        color: const Color(0xff73AEDA),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              //if searching is false, display document id of all documents in the collection 'exams' else search for the document id entered in the search bar
+              _isSearching && _searchController.text.isNotEmpty
+                  ? StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('exams')
+                          .doc(_searchController.text.toUpperCase())
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<DocumentSnapshot> snapshot) {
+                        if (snapshot.hasData && snapshot.data!.exists == true) {
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Container(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.7,
+                                    height: 55,
+                                    decoration: ShapeDecoration(
+                                      shape: RoundedRectangleBorder(
+                                        side: const BorderSide(
+                                            width: 1, color: Color(0xFFBFDAEF)),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 10),
+                                      child: Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Text(
+                                          snapshot.data!.id,
+                                          style: const TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'Montserrat',
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async {
+                                      final String docId = snapshot.data!.id;
+                                      // Replace with the actual user email
+
+                                      final DocumentReference docRef =
+                                          FirebaseFirestore.instance
+                                              .collection('userdata')
+                                              .doc(FirebaseAuth
+                                                  .instance.currentUser!.email)
+                                              .collection('exams')
+                                              .doc(docId);
+
+                                      final DocumentSnapshot docSnap =
+                                          await docRef.get();
+
+                                      if (!docSnap.exists) {
+                                        await docRef.set({
+                                          'exam': docId,
+                                        });
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(const SnackBar(
+                                          backgroundColor: Color(0xffff8800),
+                                          content: Text('Exam added!'),
+                                        ));
+                                      } else {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(const SnackBar(
+                                          backgroundColor: Color(0xffff8800),
+                                          content: Text('Exam already exists!'),
+                                        ));
+                                      }
+                                    },
+                                    child: Container(
+                                      height: 55,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.13,
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xff73AEDA),
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: const Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Icon(
+                                            Icons.add,
+                                            color: Colors.white,
+                                          ),
+                                          Text(
+                                            'Add',
+                                            style: TextStyle(
+                                                fontSize: 10,
+                                                color: Colors.white,
+                                                fontFamily: 'Montserrat',
+                                                fontWeight: FontWeight.w400),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 20),
+                            ],
+                          );
+                        } else if (snapshot.hasData &&
+                            snapshot.data!.exists == false) {
+                          return const Center(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'No exams found!',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.w400),
+                                ),
+                                Text(
+                                  'Rephrase your search query.',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ],
+                            ),
+                          );
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    )
+                  : StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('exams')
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasData) {
+                          return Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: snapshot.data!.docs
+                                .map((DocumentSnapshot document) {
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.7,
+                                        height: 55,
+                                        decoration: ShapeDecoration(
+                                          shape: RoundedRectangleBorder(
+                                            side: const BorderSide(
+                                                width: 1,
+                                                color: Color(0xFFBFDAEF)),
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 10),
+                                          child: Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text(
+                                              document.id,
+                                              style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontFamily: 'Montserrat',
+                                                  fontWeight: FontWeight.w400),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      //gesture detector with add icon
+                                      GestureDetector(
+                                        onTap: () async {
+                                          final String docId = document.id;
+                                          // Replace with the actual user email
+
+                                          final DocumentReference docRef =
+                                              FirebaseFirestore.instance
+                                                  .collection('userdata')
+                                                  .doc(FirebaseAuth.instance
+                                                      .currentUser!.email)
+                                                  .collection('exams')
+                                                  .doc(docId);
+
+                                          final DocumentSnapshot docSnap =
+                                              await docRef.get();
+
+                                          if (!docSnap.exists) {
+                                            await docRef.set({
+                                              'exam': docId,
+                                            });
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(const SnackBar(
+                                              backgroundColor:
+                                                  Color(0xffff8800),
+                                              content: Text('Exam added!'),
+                                            ));
+                                          } else {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(const SnackBar(
+                                              backgroundColor:
+                                                  Color(0xffff8800),
+                                              content:
+                                                  Text('Exam already exists!'),
+                                            ));
+                                          }
+                                        },
+                                        child: Container(
+                                          height: 55,
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.13,
+                                          decoration: BoxDecoration(
+                                            color: const Color(0xff73AEDA),
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                          child: const Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Icon(
+                                                Icons.add,
+                                                color: Colors.white,
+                                              ),
+                                              Text(
+                                                'Add',
+                                                style: TextStyle(
+                                                    fontSize: 10,
+                                                    color: Colors.white,
+                                                    fontFamily: 'Montserrat',
+                                                    fontWeight:
+                                                        FontWeight.w400),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 20),
+                                ],
+                              );
+                            }).toList(),
+                          );
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      },
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
