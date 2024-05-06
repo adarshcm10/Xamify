8eae20b578266135f806e3b462779bd7891af6c7
Adarsh C M <11200049@rist.edu.in>
2024-05-06 11:49:06 +0530

    remove permission_handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:xamify/authpage.dart';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
