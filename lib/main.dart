17e6bff66d748e6179d1744224b4994635a97d51
Adarsh C M <11200049@rist.edu.in>
2024-05-06 11:48:15 +0530

    remove flutter_downloader

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:xamify/authpage.dart';
 
@@ -28,15 +27,12 @@ Future<void> main() async {
     provisional: false,
     sound: true,
   );
-  // Initialize the flutter_downloader package
-  await FlutterDownloader.initialize(
-      debug: true // optional: set false to disable printing logs to console
-      );
-  // Ask for storage permission
-  var status = await Permission.storage.status;
-  if (!status.isGranted) {
-    await Permission.storage.request();
-  }
+  //ask notification permission
+  await messaging.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
 
   runApp(const MyApp());
 }
