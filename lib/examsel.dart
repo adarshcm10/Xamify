3cfa7098dbd20499e25d6f3cc1f4c85f4cdb1bb9
Adarsh C M <11200049@rist.edu.in>
2024-05-07 11:44:45 +0530

    route fix

--- lib/examsel.dart
+++ lib/examsel.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 //messaging
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:xamify/home.dart';
+import 'package:xamify/navbar.dart';
 import 'package:xamify/transitions.dart';
 
 class ExamSelect extends StatefulWidget {
@@ -115,7 +115,7 @@ class _ExamSelectState extends State<ExamSelect> {
                       }
 
                       Navigator.pushReplacement(
-                          context, EnterRoute(page: const HomePage()));
+                          context, EnterRoute(page: const NavBar()));
                     } else {
                       //snackbar if no exam is selected
                       ScaffoldMessenger.of(context).showSnackBar(
